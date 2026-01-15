9a9f417d04f074f3569245f318d3b865453ef429
Manas Deka <sunnymdx@gmail.com>
2026-01-15 16:44:41 +0530

    SearchUsingTavilyand creating own tool for it

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 from dotenv import load_dotenv
 import os
 from langchain_core.prompts import PromptTemplate
-from langchain_ollama import ChatOllama
 
 load_dotenv()
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_ollama import ChatOllama
+from tavily import TavilyClient
 
+tavily = TavilyClient()
+@tool
+def search(query: str) -> str:
+    """
+    Tool that searches over internet
+    Args:
+        query: The query to search for
+    Returns:
+        The search results
+    """
+    print(f"Searching for {query}")
+    return tavily.search(query=query)
+
+llm = ChatOllama(model="gpt-oss:20b")
+tools = [search]
+agent = create_agent(model=llm, tools=tools)
 def main():
-    print(os.getenv("OPENAI_API_KEY"))
+    #print(os.getenv("OPENAI_API_KEY"))
     print("Hello from langchain-course!")
-    
-
+    result = agent.invoke({"messages":HumanMessage(content="What is the weather in Tokyo?")})
+    print(result)
 
 if __name__ == "__main__":
     main()
